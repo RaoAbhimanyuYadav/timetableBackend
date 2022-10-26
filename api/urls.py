4e852e0c92c5bccfdb826701e2976d6b18df88f6
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-10-26 13:00:29 +0530

    access & refresh token Api endpoint created

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from . import views
+
 
 urlpatterns = [
     path("", views.getRoutes, name='routes'),
@@ -9,5 +15,8 @@ urlpatterns = [
     path("course/", views.courseView, name='course'),
     path("department/", views.departmentView, name='department'),
     path("section/", views.sectionView, name='section'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
