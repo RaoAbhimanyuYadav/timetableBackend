225653362389f8238a7772d279b635fd9f127c86
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-11-10 20:31:39 +0530

    Added token verify api and data

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+    TokenVerifyView
 )
 
 from . import views
@@ -16,7 +17,7 @@ urlpatterns = [
     path("subject/", views.subjectView, name='subject'),
     # path("generate-timetable/", views.generateTimeTableView,
     #      name='generate-timetable'),
-
+    path('login/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('login/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
