4886021cd6dc56652bac7a0eacc48ff4e1883c79
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2022-11-11 17:48:15 +0530

    Group model created

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Timing, Professor, Year, Subject
+from .models import Timing, Professor, Year, Subject, Group
 # Register your models here.
 
 admin.site.register(Timing)
 admin.site.register(Professor)
 admin.site.register(Year)
 admin.site.register(Subject)
+admin.site.register(Group)
--- timetable/models.py
+++ timetable/models.py
@@ -6,7 +6,7 @@ import uuid
 
 class Timing(models.Model):
     owner = models.ForeignKey(
-        User, blank=True, null=True, on_delete=models.SET_NULL)
+        User, blank=True, null=True, on_delete=models.CASCADE)
     day = models.CharField(max_length=10, unique=True)
     start_time = models.TimeField(blank=True, null=True)
     end_time = models.TimeField(blank=True, null=True)
@@ -22,7 +22,7 @@ class Timing(models.Model):
 
 class Professor(models.Model):
     owner = models.ForeignKey(
-        User, blank=True, null=True, on_delete=models.SET_NULL)
+        User, blank=True, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=250, blank=True, null=True)
     nick_name = models.CharField(max_length=10, unique=True)
     id = models.UUIDField(default=uuid.uuid4,
@@ -34,9 +34,10 @@ class Professor(models.Model):
 
 class Year(models.Model):
     owner = models.ForeignKey(
-        User, blank=True, null=True, on_delete=models.SET_NULL)
+        User, blank=True, null=True, on_delete=models.CASCADE)
     semester = models.IntegerField(unique=True)
     room = models.CharField(max_length=25, blank=True, null=True)
+    total_groups = models.IntegerField(default=1)
     id = models.UUIDField(default=uuid.uuid4,
                           primary_key=True, unique=True, editable=False)
 
@@ -46,11 +47,9 @@ class Year(models.Model):
 
 class Subject(models.Model):
     owner = models.ForeignKey(
-        User, blank=True, null=True, on_delete=models.SET_NULL)
+        User, blank=True, null=True, on_delete=models.CASCADE)
     name = models.CharField(max_length=250, blank=True, null=True)
     code = models.CharField(max_length=25, unique=True)
-    lecture_in_a_week = models.IntegerField(default=1)
-    slot_required = models.IntegerField(default=1)
     teacher = models.ForeignKey(
         Professor, on_delete=models.CASCADE, blank=True, null=True)
     year = models.ForeignKey(
@@ -61,3 +60,21 @@ class Subject(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.code})"
+
+
+class Group(models.Model):
+    owner = models.ForeignKey(
+        User, blank=True, null=True, on_delete=models.CASCADE)
+
+    lecture_in_a_week = models.IntegerField(default=1)
+    slot_required = models.IntegerField(default=1)
+    group_number = models.ForeignKey(
+        Subject, on_delete=models.CASCADE, blank=True, null=True)
+    id = models.UUIDField(default=uuid.uuid4,
+                          primary_key=True, unique=True, editable=False)
+
+    def __str__(self):
+        return f"{self.name} ({self.code})"
+# Group Number = 0 => Whole class
+# Group Number = 1 => G1
+# Group Number = 2 => G2
