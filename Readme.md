372ad88a8cdd5458351ea960ff652350aa6ee7ab
Abhimanyu Yadav <raoyadavabhi@gmail.com>
2023-05-11 11:57:49 +0530

    Shifted to Postgres DB

--- Readme.md
+++ Readme.md
@@ -10,3 +10,13 @@ activate env
 pip install -r requirements.txt
 
 python manage.py runserver
+
+DATA MIGRATION
+Remember to off Signal and after migration turn on
+
+python manage.py dumpdata > data.json
+python manage.py dumpdata --exclude auth.permission --exclude contenttypes > db.json
+
+python manage.py flush
+
+python manage.py loaddata db.json
--- timetableBackend/settings.py
+++ timetableBackend/settings.py
@@ -104,8 +104,14 @@ WSGI_APPLICATION = 'timetableBackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'timetable',
+        'USER': 'postgres',
+        'PASSWORD': 'Admin123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
